7cd1b39f3090432617cf85c45043edcc2927be66
ishreet <ishreetb@gmail.com>
2022-04-15 04:04:34 -0400

    changed build folder

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 const calendarTranspile = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid'
+	'@fullcalendar/common',
+	'@fullcalendar/react',
+	'@fullcalendar/daygrid',
+	'@fullcalendar/list',
+	'@fullcalendar/timegrid'
 ]);
 
 const withImages = require('next-images');
 
+module.exports = { distDir: 'out' };
+
 module.exports = withImages(
-  calendarTranspile({
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en']
-    }
-  })
+	calendarTranspile({
+		i18n: {
+			defaultLocale: 'en',
+			locales: ['en']
+		}
+	})
 );
